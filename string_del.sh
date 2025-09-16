d86ed7b1986be6f2320abcb04d068e61043950e3
Serj Zaytsev <86710738+serjiy@users.noreply.github.com>
2025-09-16 13:50:05 +0300

    Update and rename task #4 to string_del.sh
    Added Unicode support. Cut has been replaced with the awk command

--- string_del.sh
+++ string_del.sh
@@ -1,3 +1,5 @@
+# This script is designed to select or delete a substring from a given string based on the specified start and end positions.
+
 #!/bin/bash
 
 # Проверка количества аргументов
@@ -13,13 +15,24 @@ end="$3"
 delete_mode=false
 
 # Проверка, активирован ли режим удаления
-if [ $# -eq 4 ] && [ "$4" = "-d" ]; then
-    delete_mode=true
+if [ $# -eq 4 ]; then
+    if [ "$4" = "-d" ]; then
+        delete_mode=true
+    else
+        echo "Ошибка: неверный флаг. Используйте -d для режима удаления."
+        exit 1
+    fi
 fi
 
 # Проверка, что позиции - числа
 if ! [[ "$start" =~ ^[0-9]+$ ]] || ! [[ "$end" =~ ^[0-9]+$ ]]; then
-    echo "Ошибка: начальная и конечная позиции должны быть числами"
+    echo "Ошибка: начальная и конечная позиции должны быть положительными числами"
+    exit 1
+fi
+
+# Проверка, что позиции не меньше 1
+if [ "$start" -lt 1 ] || [ "$end" -lt 1 ]; then
+    echo "Ошибка: позиции должны быть не меньше 1"
     exit 1
 fi
 
@@ -29,20 +42,24 @@ if [ "$start" -gt "$end" ]; then
     exit 1
 fi
 
-if [ "$end" -gt "${#string}" ]; then
+# Получение длины строки в символах (поддержка Unicode)
+str_length=$(printf "%s" "$string" | wc -m)
+if [ "$end" -gt "$str_length" ]; then
     echo "Ошибка: конечная позиция превышает длину строки"
     exit 1
 fi
 
-# Выделение или удаление подстроки
+# Выделение или удаление подстроки с поддержкой Unicode
 if [ "$delete_mode" = true ]; then
     # Режим удаления: выводим части до и после указанного диапазона
-    part1=$(echo "$string" | cut -c 1-$((start-1)))
-    part2=$(echo "$string" | cut -c $((end+1))-)
-    result="${part1}${part2}"
+    result=$(printf "%s" "$string" | awk -v start="$start" -v end="$end" '{
+        print substr($0, 1, start-1) substr($0, end+1)
+    }')
 else
     # Режим выделения: выводим только указанный диапазон
-    result=$(echo "$string" | cut -c "$start"-"$end")
+    result=$(printf "%s" "$string" | awk -v start="$start" -v end="$end" '{
+        print substr($0, start, end-start+1)
+    }')
 fi
 
 echo "$result"
