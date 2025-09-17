9ade8b562c1bd58cde6352f761fc12af9de8b38a
Serj Zaytsev <86710738+serjiy@users.noreply.github.com>
2025-09-17 12:17:28 +0300

    Update string_del.sh

--- string_del.sh
+++ string_del.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
 # This script is designed to select or delete a substring from a given string based on the specified start and end positions.
+
+
 
 
 # Проверка количества аргументов
