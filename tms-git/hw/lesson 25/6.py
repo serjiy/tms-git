8c72890bdf701dc69afec0cb00523aa0af65a75f
Serj Zaytsev <86710738+serjiy@users.noreply.github.com>
2025-12-26 10:31:16 +0300

    Addes new HW files

---
+++ tms-git/hw/lesson 25/6.py
@@ -0,0 +1,4 @@
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+product = num1 * num2
+print(f"Произведение: {product}")
---
+++ tms-git/hw/lesson 25/8.py
@@ -0,0 +1,3 @@
+number = int(input("Введите целое число: "))
+square = number ** 2
+print(f"Квадрат числа: {square}")
