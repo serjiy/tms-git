9b96f688427ff9c309777f64cc9972a4a748d95b
Serj Zaytsev <86710738+serjiy@users.noreply.github.com>
2025-12-29 13:45:48 +0300

    modified:   tms-git/hw/lessom 26/ping_test.py

--- tms-git/hw/lessom 26/ping_test.py
+++ tms-git/hw/lessom 26/ping_test.py
@@ -5,9 +5,9 @@ import platform
 def check_ip_ping(ip_list, output_file="ping_results.txt"):
     with open(output_file, "w", encoding="utf-8") as f:
         for ip in ip_list:
-            # Определяем параметр для ping в зависимости от ОС
+            # Определяет параметр для ping в зависимости от ОС
             param = "-n" if platform.system().lower() == "windows" else "-c"
-            # Выполняем ping (1 пакет)
+            # Выполняет ping (1 пакет)
             result = subprocess.run(["ping", param, "1", ip], 
                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             status = "ДОСТУПЕН" if result.returncode == 0 else "НЕДОСТУПЕН"
